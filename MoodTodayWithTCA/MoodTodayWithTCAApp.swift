ca235a5e796001d48f64a5f253650f58bb8a6eca
CentLee <step323@naver.com>
2022-07-16 15:43:12 +0900

    Initial Commit

---
+++ MoodTodayWithTCA/MoodTodayWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTodayWithTCAApp.swift
+//  MoodTodayWithTCA
+//
+//  Created by 이창화 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct MoodTodayWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
